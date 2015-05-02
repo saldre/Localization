bbb1f9a86dc6c336d09369f7aff6518a9fd54bdf
saldre <mattias.saldre@gmail.com>
2015-05-02 18:38:57 +0300

    Revert "Improved support for dynamic content creation."
    This reverts commit 2b1bdfe6714ebf15e79d0d49a3707abbe5facd84.

--- localization.js
+++ localization.js
@@ -88,8 +88,7 @@ Translation.UPDATE_EVENT = 'translationupdate.localization';
  * @param {jQuery} $element
  */
 Translation.prototype.setElement = function($element) {
-  this.$element = this.$element
-    : $element;
+  this.$element = $element;
 
   // Automatically update the content if the translation changes.
   this.$element.on(Translation.UPDATE_EVENT, this.updateElement.bind(this));
