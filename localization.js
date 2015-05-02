4d450d8013f9c611fed7cc1635e9ae296aa99718
saldre <mattias.saldre@gmail.com>
2015-05-02 18:58:16 +0300

    Data attribute's name can now be modified.
    Default attribute name changed to data-translate.

--- localization.js
+++ localization.js
@@ -7,6 +7,9 @@ var Localization = (function() {
   // Default language. Override via setLanguage whenever needed.
   var _language = 'ENG';
 
+  // Attribute holding the Translation object's ID.
+  var _dataAttribute = 'data-translate';
+
   /**
    * @param {String} [method]
    * @constructor
@@ -16,6 +19,102 @@ var Localization = (function() {
   }
 
   InvalidTranslationTypeError.prototype = Error.prototype;
+
+
+  /**
+   * @param {String} id
+   * @param {Object} languages
+   * @constructor
+   */
+  window.Translation = function(id, languages) {
+
+    if ( !Localization.getTranslation(id) ) {
+      this.id = id;
+      this.setElement($('[' + _dataAttribute + '="' + id + '"]'));
+
+      Localization.storeTranslation(this);
+    }
+
+    Localization.getTranslation(id).updateLanguages(languages);
+  };
+
+  Translation.UPDATE_EVENT = 'translationupdate.localization';
+
+
+  /**
+   * @param {jQuery} $element
+   */
+  Translation.prototype.setElement = function($element) {
+    this.$element = this.$element
+      ? this.$element.add($element)  // Keep the previous elements using the same Translation.
+      : $element;
+
+    // Automatically update the content if the translation changes.
+    this.$element.on(Translation.UPDATE_EVENT, this.updateElement.bind(this));
+  };
+
+
+  /**
+   * @param {Object} languages
+   */
+  Translation.prototype.updateLanguages = function(languages) {
+
+    if ( !this.languages ) {
+      this.languages = {};
+    }
+
+    $.each(languages, function(language, text) {
+      this.languages[language] = text;
+    }.bind(this));
+
+    this.$element.trigger(Translation.UPDATE_EVENT);
+  };
+
+
+  /**
+   * Updates element's contents according to the selected language.
+   */
+  Translation.prototype.updateElement = function() {
+    this.$element.html(this.getText());
+  };
+
+
+  /**
+   * @param {String} [language]
+   * @return {String}
+   */
+  Translation.prototype.getText = function(language) {
+    return this.languages[language || Localization.getLanguage()];
+  };
+
+
+  /**
+   * @param {String} [translationId]
+   * @param {Object} [languages]
+   * @return {jQuery}
+   */
+  jQuery.fn.translate = function(translationId, languages) {
+    var $element = this;
+    var translation;
+
+    if ( translationId ) {
+      translation = new Translation(translationId, languages);
+      $element.attr(_dataAttribute, translationId);
+    }
+
+    if ( $element.attr(_dataAttribute) ) {
+      translationId = $element.attr(_dataAttribute);
+      translation = Localization.getTranslation(translationId);
+    }
+    else {
+      throw new Error('Element missing translation data.');
+    }
+
+    translation.setElement($element);
+    $element.trigger(Translation.UPDATE_EVENT);
+
+    return $element;
+  };
 
   // Public interface.
   return {
@@ -28,7 +127,7 @@ var Localization = (function() {
       _language = language;
 
       // Let all localized content know that the language has been changed.
-      $('[data-label]').trigger(Translation.UPDATE_EVENT);
+      $('[' + _dataAttribute + ']').trigger(Translation.UPDATE_EVENT);
     },
 
 
@@ -37,6 +136,14 @@ var Localization = (function() {
      */
     getLanguage: function() {
       return _language;
+    },
+
+
+    /**
+     * @param {String} dataAttribute
+     */
+    setDataAttribute: function(dataAttribute) {
+      _dataAttribute = dataAttribute;
     },
 
 
@@ -60,101 +167,5 @@ var Localization = (function() {
     getTranslation: function(translationId) {
       return _cache[translationId];
     }
-  }
+  };
 })();
-
-
-/**
- * @param {String} id
- * @param {Object} languages
- * @constructor
- */
-function Translation(id, languages) {
-
-  if ( !Localization.getTranslation(id) ) {
-    this.id = id;
-    this.setElement($('[data-label="' + id + '"]'));
-
-    Localization.storeTranslation(this);
-  }
-
-  Localization.getTranslation(id).updateLanguages(languages);
-}
-
-Translation.UPDATE_EVENT = 'translationupdate.localization';
-
-
-/**
- * @param {jQuery} $element
- */
-Translation.prototype.setElement = function($element) {
-  this.$element = this.$element
-    ? this.$element.add($element)  // Keep the previous elements using the same Translation.
-    : $element;
-
-  // Automatically update the content if the translation changes.
-  this.$element.on(Translation.UPDATE_EVENT, this.updateElement.bind(this));
-};
-
-
-/**
- * @param {Object} languages
- */
-Translation.prototype.updateLanguages = function(languages) {
-
-  if ( !this.languages ) {
-    this.languages = {};
-  }
-
-  $.each(languages, function(language, text) {
-    this.languages[language] = text;
-  }.bind(this));
-
-  this.$element.trigger(Translation.UPDATE_EVENT);
-};
-
-
-/**
- * Updates element's contents according to the selected language.
- */
-Translation.prototype.updateElement = function() {
-  this.$element.html(this.getText());
-};
-
-
-/**
- * @param {String} language
- * @return {String}
- */
-Translation.prototype.getText = function(language) {
-  return this.languages[language || Localization.getLanguage()];
-};
-
-
-/**
- * @param {String} [translationId]
- * @param {Object} [languages]
- * @return {jQuery}
- */
-jQuery.fn.translate = function(translationId, languages) {
-  var $element = this;
-  var translation;
-
-  if ( translationId ) {
-    translation = new Translation(translationId, languages);
-    $element.attr('data-label', translationId);
-  }
-
-  if ( $element.data('label') ) {
-    translationId = $element.data('label');
-    translation = Localization.getTranslation(translationId);
-  }
-  else {
-    throw new Error('Element missing translation data.');
-  }
-
-  translation.setElement($element);
-  $element.trigger(Translation.UPDATE_EVENT);
-
-  return $element;
-};
